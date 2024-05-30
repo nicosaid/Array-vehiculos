0c0d206a22cab40c0554b426de98da058ace4383
nicosaid <nico.said.r@gmail.com>
2024-05-30 15:52:14 -0300

    vehiculosarray

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+    public int vehiculorandom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            vehiculorandom = Random.RandomRange(0, vehiculos.Length);
+            DesactivarTodosLosVehiculos();
+            vehiculos[vehiculorandom].SetActive(true);
+
+        }
+    }
+
+    void ResetearVehiculos()
+    {
+
+        DesactivarTodosLosVehiculos();
+        vehiculos[0].SetActive(true);
+
+    }
+    void DesactivarTodosLosVehiculos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+
+}
